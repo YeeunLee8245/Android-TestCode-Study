f34f55811950a133563e56e91ff0ecb7d04f5dc7
Yeeun Lee <leeyeah8245@gmail.com>
2023-04-18 15:32:29 +0900

    [Add Codes] 세금 class Before, After annotation

--- app/src/main/java/kr/co/yeeun/lee/demoi/android_testcode_study/MainActivity.kt
+++ app/src/main/java/kr/co/yeeun/lee/demoi/android_testcode_study/MainActivity.kt
@@ -8,11 +8,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tax = Tax()
-        val netIncome = tax.calculateIncome(100.0, 0.1) // 소득: 100, 세율: 10%
-        val netTax = tax.calcualteTax(100.0, 0.1)
-
-        println(netIncome)
-        println(netTax)
     }
+
+//    private fun calculateTax() {    // 세금 계산 Unit Test 관련 예시(test 모듈 TestTax Class 참조)
+//        val tax = Tax()
+//        val netIncome = tax.calculateIncome(100.0, 0.1) // 소득: 100, 세율: 10%
+//        val netTax = tax.calcualteTax(100.0, 0.1)
+//
+//        println(netIncome)
+//        println(netTax)
+//    }
 }
--- app/src/test/java/kr/co/yeeun/lee/demoi/android_testcode_study/TestTax.kt
+++ app/src/test/java/kr/co/yeeun/lee/demoi/android_testcode_study/TestTax.kt
@@ -1,20 +1,30 @@
 package kr.co.yeeun.lee.demoi.android_testcode_study
 
 import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 class TestTax { // class 단위로 class 내부 메소드 한 번에 수행 가능
 
+    private lateinit var tax : Tax
+
+    @Before // 테스트 초기화 전 수행
+    fun setup() {
+        tax = Tax()
+    }
+
+    @After // 테스트 후 수행
+    fun teardown() {}
+
     @Test // 테스트용으로 작성
     fun calculateTaxTest() { // 테스트 이름은 누구나 목적을 파악할 수 있게 작성할 것, 테스트 코드에 한 해 백틱으로 공백이 있는 메소드 작성 가능
-        val tax = Tax()
         val netTax = tax.calcualteTax(100.0, 0.1)
         assertThat(netTax).isEqualTo(10) // netTax가 10과 동일한지 테스트
-    }
+    } // 각 테스트에서 오직 하나의 assertion만 가져야함, 상황상 그럴 수 없을 순 있지만 이것을 최대한 지키자(분할하는 방법으로)
 
     @Test
     fun calculateIncomeTest() {
-        val tax = Tax()
         val netIncome = tax.calculateIncome(100.0, 0.1)
         assertThat(netIncome).isEqualTo(90)
     }
